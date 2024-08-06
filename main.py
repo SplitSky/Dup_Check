d7b38ede6ae602bd254cd7503da7eae171ed220e
SplitSky <opop.neska60@gmail.com>
2024-08-06 11:45:26 +0100

    Fixed the config file formatting. Keep it at utf-8. Added structure and reading config and loading csv functions@

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import numpy as np
 import csv
 from math import floor
 
@@ -23,7 +22,7 @@ def jaro_distance(s1, s2) :
 	len1 = len(s1);
 	len2 = len(s2); 
 	if (len1 == 0 or len2 == 0) :
-		return 0.0; 
+		return 0.0;
 	# Maximum distance upto which matching 
 	# is allowed 
 	max_dist = (max(len(s1), len(s2)) // 2 ) - 1; 
@@ -99,9 +98,53 @@ def JW_score(s1, s2) :
 		jaro_dist += 0.1 * prefix * (1 - jaro_dist); 
 	return jaro_dist; 
 
-
+def read_csv(file_path):
+    """
+    Reads a CSV file into a list of dictionaries for data management.
+    :param file_path: str, path to the CSV file
+    :return: list of dictionaries
+    """
+    data = []
+    
+    with open(file_path, mode='r', encoding='utf-8-sig') as file:
+        # Create a CSV reader object
+        csv_reader = csv.DictReader(file)
+        # Iterate over each row and append it to the data list
+        for row in csv_reader:
+            data.append(row)
+            
+    return data
+            
+def load_config(config_path):
+	# Loads the config csv and uses it for data calculations
+	# Use the keys for the list of fields that are available in the CSV
+	data = {}
+	with open(config_path, mode='r', encoding='utf-8-sig') as file:
+		# Create a CSV reader object
+		csv_reader = csv.DictReader(file)
+		# Iterate over each row and append it to the data list
+		first = True
+		for row in csv_reader:
+			if first:
+				first = False
+			else:
+				# compile the row: key : [null, weighting]
+				data[row['Name']] = [row['null_score'], row['weighting_score']]
+		print(data)
+		file.close()
+	return data
+            
+            
+def DUNS_score(path_to_data):
+    config_dict = load_config("config.csv") # stores all of the config data to be used in weighting and nulls
+    # Modifies the data dicitonary to include a score column and adds the scores there
+    # For pair of fields -> load the csv
+    data = read_csv(path_to_data)
+    for i in range(0,len(data),1):
+        # loop over each pair
 # define main
 def main():
+
     # import the CSV
     # import the config weights and null values
         # This is a csv containing all of the editable variables
